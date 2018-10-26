0f4c7edd48114a6b56e491d2bc74c745dcb27334
Mostafa <dj.mostafa27@gmail.com>
2018-10-26 20:06:43 +0330

    bia

--- app/src/main/java/com/ba/dota/PayPage.java
+++ app/src/main/java/com/ba/dota/PayPage.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 public class PayPage extends AppCompatActivity {
 
@@ -14,7 +20,26 @@ public class PayPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_page);
 
+        final ImageView imageView = (ImageView) findViewById(R.id.image_info);
 
+
+
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+        View view = inflater.inflate(R.layout.popup_info,null);
+        final PopupWindow popupWindow = new PopupWindow(view,width,height,true);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.showAtLocation(imageView,Gravity.CENTER,0,0);
+
+            }
+        });
     }
 
 
