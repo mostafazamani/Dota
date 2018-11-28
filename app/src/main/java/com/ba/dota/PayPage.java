4421824f25c0c8ec3162047fdf6faeadc5d43c4f
Mostafa <dj.mostafa27@gmail.com>
2018-11-28 21:12:58 +0330

    bia

--- app/src/main/java/com/ba/dota/PayPage.java
+++ app/src/main/java/com/ba/dota/PayPage.java
@@ -12,8 +12,10 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class PayPage extends AppCompatActivity {
+    TextView amount , payer_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +23,26 @@ public class PayPage extends AppCompatActivity {
         setContentView(R.layout.activity_pay_page);
 
         final ImageView imageView = (ImageView) findViewById(R.id.image_info);
+        amount = (TextView) findViewById(R.id.amount_show);
+        payer_name = (TextView) findViewById(R.id.payer_name);
+        String name = getIntent().getStringExtra("name");
+        int gh = getIntent().getIntExtra("gh",0);
 
-
-
+        payer_name.setText(name);
+        amount.setText(String.valueOf(gh));
         LayoutInflater inflater = (LayoutInflater)
                 getSystemService(LAYOUT_INFLATER_SERVICE);
 
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
 
-        View view = inflater.inflate(R.layout.popup_info,null);
-        final PopupWindow popupWindow = new PopupWindow(view,width,height,true);
+        View view = inflater.inflate(R.layout.popup_info, null);
+        final PopupWindow popupWindow = new PopupWindow(view, width, height, true);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                popupWindow.showAtLocation(imageView,Gravity.CENTER,0,0);
+                popupWindow.showAtLocation(imageView, Gravity.CENTER, 0, 0);
 
             }
         });
