31c4cca3d2c3a673a265fb422f371eeb2570e4f1
Vahid Aali <aalipour.vahid@gmail.com>
2018-12-15 21:33:37 +0330

    store

--- app/src/main/java/com/ba/dota/Chest.java
+++ app/src/main/java/com/ba/dota/Chest.java
@@ -1,13 +1,27 @@
 package com.ba.dota;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Chest extends AppCompatActivity {
 
+    Button chest_pay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chest);
+
+        chest_pay = (Button) findViewById(R.id.chest_pay);
+
+                chest_pay.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(Chest.this,ChestPay.class));
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/ba/dota/ChestPay.java
@@ -0,0 +1,13 @@
+package com.ba.dota;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChestPay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chest_pay);
+    }
+}
