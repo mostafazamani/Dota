a534299e5a6549847f4a952875ad90e18b64f81d
Mostafa <dj.mostafa27@gmail.com>
2018-12-28 12:59:26 +0330

    pay

--- app/src/main/java/com/ba/dota/SetsenderChest.java
+++ app/src/main/java/com/ba/dota/SetsenderChest.java
@@ -1,9 +1,9 @@
 package com.ba.dota;
 
-import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,74 +23,57 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
-public class SetSender extends DialogFragment {
+public class SetsenderChest extends DialogFragment {
 
     TextView text;
     Button btn_ok, btn_ref;
     ProgressBar prog;
 
-    public static final String uri = "";
+    public static final String url = "";
 
-    public static SetSender newinstance(String name, String itemname, String tradelink, String numberphone, String refid,int x) {
-
-        SetSender sender = new SetSender();
-
+    public static final SetsenderChest chestinstance(int numberchest, String refid, int x) {
+        SetsenderChest chest = new SetsenderChest();
         Bundle bundle = new Bundle();
-
-        bundle.putString("n", name);
-        bundle.putString("i", itemname);
-        bundle.putString("t", tradelink);
-        bundle.putString("np", numberphone);
-        bundle.putString("ri", refid);
-        bundle.putInt("x",x);
-
-        sender.setArguments(bundle);
+        bundle.putInt("numberchest", numberchest);
+        bundle.putString("refid", refid);
+        bundle.putInt("x", x);
 
 
-        return sender;
+        return chest;
     }
 
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-
-        final View view = inflater.inflate(R.layout.set_sender, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.set_sender, container, false);
 
         text = (TextView) view.findViewById(R.id.txt_sender);
         btn_ok = (Button) view.findViewById(R.id.ok_sender);
         btn_ref = (Button) view.findViewById(R.id.ref_sender);
         prog = (ProgressBar) view.findViewById(R.id.prog_sender);
 
-
-        //  btn_ok.setVisibility(View.INVISIBLE);
         btn_ref.setVisibility(View.INVISIBLE);
         prog.setVisibility(View.INVISIBLE);
 
-        String name = getArguments().getString("n");
-        String itemname = getArguments().getString("i");
-        String tradelink = getArguments().getString("t");
-        String numberphone = getArguments().getString("np");
-        final String refid = getArguments().getString("ri");
+        final int numberchest = getArguments().getInt("numberchest");
+        final String refid = getArguments().getString("refid");
         final int[] x = {getArguments().getInt("x")};
 
-        text.setText("خرید شما با موفقیت انجام شد" +
-                "\n" +
-                "کدرهگیری:" +
-       refid );
+        final int numberitem = setrandom();
+
 
         final RequestQueue queue = Volley.newRequestQueue(getActivity());
 
         final Map<String, String> param = new HashMap<String, String>();
 
-        param.put("name", name);
-        param.put("itemname", itemname);
-        param.put("tradelink", tradelink);
-        param.put("numberphone", numberphone);
+        param.put("numberchest", String.valueOf(numberchest));
+        param.put("numberitem", String.valueOf(numberitem));
         param.put("refid", refid);
 /*
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, uri, new JSONObject(param), new Response.Listener<JSONObject>() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(param), new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 text.setText("خرید تکمیل شد");
@@ -122,12 +105,17 @@ public class SetSender extends DialogFragment {
         };
 
         queue.add(request);
-*/
 
+*/
         btn_ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(),MainActivity.class));
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.putExtra("numberchest",numberchest);
+                intent.putExtra("numberitem",numberitem);
+
+                startActivity(intent);
+
                 getActivity().finish();
 
 
@@ -141,7 +129,7 @@ public class SetSender extends DialogFragment {
 
                 text.setText("درحال تکمیل خرید....");
 
-                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, uri, new JSONObject(param), new Response.Listener<JSONObject>() {
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(param), new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         text.setText("خرید تکمیل شد");
@@ -153,7 +141,7 @@ public class SetSender extends DialogFragment {
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        text.setText("خطا رد تکمیل خرید");
+                        text.setText("خطا در تکمیل خرید");
                         prog.setVisibility(View.INVISIBLE);
 
                         if (x[0] < 3) {
@@ -188,4 +176,28 @@ public class SetSender extends DialogFragment {
 
         return view;
     }
+
+    public int setrandom() {
+        Random random = new Random();
+        int x = random.nextInt(100);
+
+
+        if (x == 13 || x == 21 || x == 67 || x == 93) {
+            return 4;
+        } else if (x == 2 || x == 5 || x == 7 || x == 35 || x == 42 || x == 53 || x == 64 || x == 75 || x == 86 || x == 97) {
+            return 3;
+        } else if (x == 1 || x == 3 || x == 7 || x == 9 || x == 11 || x == 23 || x == 29 || x == 32 || x == 39 || x == 41 || x == 52 || x == 59 || x == 60 || x == 70 || x == 80 || x == 90 || x == 99 || x == 100 || x == 85 || x == 73 || x == 65) {
+            return 2;
+        } else {
+            if (random.nextInt(2) == 2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+
+    }
+
+
 }
